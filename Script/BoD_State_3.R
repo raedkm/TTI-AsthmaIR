8421eab18a999a0deb85d210ef2041347b7d797b
Raedkm <15216779+raedkm@users.noreply.github.com>
2019-05-01 15:37:20 -0500

    Worked on the Methods and Results paper

--- Script/BoD_State_3.R
+++ Script/BoD_State_3.R
@@ -3,14 +3,15 @@
 #Part : (03) Printing Results tables
 #Purpose: Conduct the burden modeling and estimate results by state/urban/income
 #Created by Raed Alotaibi
-#Date Created: March-12-2019
+#Date Created: April-22-2019
 #---------------------------------------------#
 
 
 
 
 
-# Results tables ----------------------------------------------------------
+
+# Summary estimates -------------------------------------------------------
 
 
 BLOCK <- burden %>%
@@ -24,7 +25,7 @@ CHILDREN <- burden %>%
   summarise(CHILDREN = sum(CHILDREN), 
             ASTHMA = sum(CASES) ,
             AC = sum(AC)) %>% 
-  mutate(AF = round((AC/ASTHMA * 100), 1)) %>% 
+  mutate(AF = round((AC/ASTHMA * 100), 1)) 
   
 
 CHILDREN_URBAN <- burden %>% 
@@ -44,19 +45,35 @@ CHILDREN_INCOME <- burden %>%
 
 
 
+# Results tables ----------------------------------------------------------
+
+
 table_a <- bind_rows(CHILDREN, CHILDREN_URBAN, CHILDREN_INCOME) %>% 
-  select(URBAN, INCOME, CHILDREN, ASTHMA, AC, AF)
+  select(URBAN, INCOME, CHILDREN, ASTHMA, AC, AF) %>% as.data.frame()
 
 table_1 <- bind_rows(BLOCK, BLOCK_U, CHILDREN, CHILDREN_URBAN, CHILDREN_INCOME) %>% 
-  select(URBAN, INCOME, n, CHILDREN) 
+  select(URBAN, INCOME, n, CHILDREN) %>% as.data.frame()
   
 table_2 <- bind_rows(CHILDREN, CHILDREN_URBAN, CHILDREN_INCOME) %>% 
-  select(URBAN, INCOME,ASTHMA) 
+  select(URBAN, INCOME,ASTHMA) %>% as.data.frame()
 
 table_3 <- bind_rows(CHILDREN, CHILDREN_URBAN, CHILDREN_INCOME) %>% 
-  select(URBAN, INCOME,AC, AF) 
+  select(URBAN, INCOME,AC, AF) %>% as.data.frame()
 
 
 POLLUT <- burden %>% 
   select(NO2) %>% 
-  summary() %>% as.matrix() 
+  summary() %>% as.data.frame() 
+
+
+
+# Printing to excel -------------------------------------------------------
+class(table_a)
+
+xlsx::write.xlsx(table_a, "Results/Table_a.xlsx", row.names = F)
+xlsx::write.xlsx(table_1, "Results/Table_1.xlsx", row.names = F)
+xlsx::write.xlsx(table_2, "Results/Table_2.xlsx", row.names = F)
+xlsx::write.xlsx(table_3, "Results/Table_3.xlsx", row.names = F)
+xlsx::write.xlsx(POLLUT, "Results/Table_4.xlsx", row.names = F)
+
+
