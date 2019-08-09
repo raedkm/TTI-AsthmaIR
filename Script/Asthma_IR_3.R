4e02c7e132853ead10f4933f6a4ecbaed9dc89e5
Raedkm <raedkma@gmail.com>
2019-08-09 00:51:30 -0500

    Removed teh burden analysis portion into different project

--- Script/Asthma_IR_3.R
+++ Script/Asthma_IR_3.R
@@ -1,99 +1,66 @@
 #---------------------------------------------#
 #Project : State specific childhood Asthma incidence rate
-#Part : (03) Estimating aggregate (2006-2010) astham PRV across states 
+#Part : (03) Producing a table of Asthma IR by state and year
 #Purpose: Conduct the burden modeling and estimate results by state
 #Created by Raed Alotaibi
-#Date Created: March-12-2019
+#Date Created: June-18-2019
 #---------------------------------------------#
 
 
+#loading libraries
+library(readxl)
+library(plyr)
+library(foreign)
+library(dplyr)
+library(xlsx)
+library(tidyr)
+library(readxl)
 
-# Asthma PRV Aggregate -----------------------------------------------------
 
-
-for(i in 2006:2010) {
-  
-  i <- as.character(i)
-  j <- i
-  i <- paste0(i, "_weighted")
-  print(i)
-  
-  Asthma_result <- read_excel("Results/Tables/Asthma_result.xlsx", sheet = i) %>% 
-    select(FIPS, State,`1_BRFSS`, `2_BRFSS`) %>% 
-    mutate(Year = j) %>%
-    mutate(SAMPLE = `1_BRFSS` + `2_BRFSS`) %>% 
-    rename(EVER =  `1_BRFSS`) %>%
-    select(FIPS, State, EVER, SAMPLE, Year) %>% 
-    na.omit()
-  
-  print(Asthma_result)
-  
-  # Binding data sets by row
-  ifelse(i == "2006_weighted",   Asthma_agg <- Asthma_result, Asthma_agg <- bind_rows(Asthma_agg, Asthma_result))
-  
-}
-
-# Aggregating the PRV by state for all years 
-Asthma_agg_PRV <- Asthma_agg %>%
-  select(-Year) %>% 
-  group_by(FIPS, State) %>% 
-  summarise_all(sum ) %>% 
-  mutate(`PRV per 100` = EVER / SAMPLE*100) %>%
-  as.data.frame()
-
-range(Asthma_agg_PRV$`PRV per 100`)
+# loading States and FIPs code
+load("Data/FIPS.R")
+FIPS$FIPS <- as.double(FIPS$FIPS) #To be used to add FIPS code and state names to data sets
 
 
 
-# Aggregating the National PRV by years
-Asthma_agg_national_year_PRV <- Asthma_agg %>%
-  group_by(Year) %>%
-  summarise_at(c("EVER", "SAMPLE"), sum ) %>%
-  mutate(`PRV per 100` = EVER / SAMPLE*100) %>%
-  as.data.frame()
+
+# Section 1 - Available years ---------------------------------------------
+
+Asthma_var <- c('State', 'IR_per1000')
+
+
+# Loading the Asthma incidence rate tables produced from earlier scripts
+Asthma_06 <- read_excel("Results/Tables/Asthma_IR.xlsx", sheet = "2006") %>% select(Asthma_var)
+Asthma_07 <- read_excel("Results/Tables/Asthma_IR.xlsx", sheet = "2007") %>% select(Asthma_var)
+Asthma_08 <- read_excel("Results/Tables/Asthma_IR.xlsx", sheet = "2008") %>% select(Asthma_var)
+Asthma_09 <- read_excel("Results/Tables/Asthma_IR.xlsx", sheet = "2009") %>% select(Asthma_var)
+Asthma_10 <- read_excel("Results/Tables/Asthma_IR.xlsx", sheet = "2010") %>% select(Asthma_var)
+Asthma_agg <- read_excel("Results/Tables/Asthma_IR.xlsx", sheet = "Aggregate") %>% select('State', `IR per 1000`)
 
 
 
-# Aggregating the National PRV 
-Asthma_agg_national_PRV <- Asthma_agg_national_year_PRV %>%
-  #group_by(Year) %>%
-  summarize(EVER = sum(EVER)/5,
-            SAMPLE = sum(SAMPLE)/5,
-            `PRV per 100` = EVER / SAMPLE*100) %>%
-  mutate(Year = "'National") %>% 
-  as.data.frame()
+# Table with IR by state and year -----------------------------------------
+
+
+Asthma_year <-  FIPS %>% 
+  left_join(Asthma_06, by = 'State') %>% 
+  left_join(Asthma_07, by = 'State') %>%   
+  left_join(Asthma_08, by = 'State') %>% 
+  left_join(Asthma_09, by = 'State') %>% 
+  left_join(Asthma_10, by = 'State') %>% 
+  left_join(Asthma_agg) %>% 
+  select(-FIPS)
+
+
+names_asthma <- c("State", "2006", "2007", "2008", "2009", "2010", "Aggregate")
+
+names(Asthma_year) <- names_asthma
+
+Asthma_year
 
 
 
-# Printing Aggregated data to Excel
-write.xlsx(Asthma_agg_PRV, "Results/Tables/Asthma_PRV.xlsx", sheetName = "Aggregate", showNA=F, append = T, row.names = F)
+# Priting the table to excel
+write.xlsx(Asthma_year, "Results/Tables/Asthma_years.xlsx",  showNA=F, append = F, row.names = F)
 
 
-
-# Printing Tables 8
-table_IR <- Asthma_agg_national_IR %>% 
-  rbind(Asthma_agg_national_year_IR) 
-  
-table_PRV <- Asthma_agg_national_PRV %>% 
-  rbind(Asthma_agg_national_year_PRV) 
-
-names <- table_IR$Year
-
-table_8 <- table_IR %>% 
-  left_join(table_PRV, by = "Year") %>% 
-  select(SAMPLE, EVER, `<12_month`, At_risk, `IR per 1000`, `PRV per 100`) %>% 
-  t()
-
-colnames(table_8) <- names 
-
-write.xlsx(table_8, "Results/Tables/table_A1.xlsx",  append = F, row.names = T)
-
-
-
-# Printing Table 9
-
-table_9 <- Asthma_agg_IR %>% 
-  left_join(Asthma_agg_PRV, by = c("FIPS", "State")) %>% 
-  select(State, SAMPLE, EVER, `<12_month`, At_risk, `IR per 1000`, `PRV per 100`)
-
-write.xlsx(table_9, "Results/Tables/table_A2.xlsx",  append = F, row.names = F)
--- Script/For_deleting/Asthma_IR_3.R
+++ Script/For_deleting/Asthma_IR_3.R
@@ -1,74 +1,10 @@
 #---------------------------------------------#
 #Project : State specific childhood Asthma incidence rate
-#Part : (02) Estimating aggregate (2006-2010) astham IR and PR across states 
+#Part : (03) Estimating aggregate (2006-2010) astham PRV across states 
 #Purpose: Conduct the burden modeling and estimate results by state
 #Created by Raed Alotaibi
 #Date Created: March-12-2019
-#Date Updated: Aug-8-2019
 #---------------------------------------------#
-
-
-# Asthma IR Aggregate -----------------------------------------------------
-
-
-
-for(i in 2006:2010) {
-  
-  i <- as.character(i)
-  print(i)
-  
-  
-  Asthma_result <- read_excel("Results/Tables/Asthma_IR.xlsx", sheet = i) %>% 
-    select(FIPS, State,`<12_month`, `At_risk`) %>% 
-    mutate(Year = i) %>% 
-    na.omit()
-  
-  print(Asthma_result)
-  
-  # Binding data sets by row
-
-
-  ifelse(i == "2006",   Asthma_agg <- Asthma_result, Asthma_agg <- bind_rows(Asthma_agg, Asthma_result))
-  
-  
-}
-
-
-# Aggregating the IR by state for all years 
-Asthma_agg_IR <- Asthma_agg %>%
-  select(-Year) %>% 
-  group_by(FIPS, State) %>% 
-  summarise_all(sum ) %>% 
-  mutate(`IR per 1000` = `<12_month`/ At_risk*1000) %>% 
-  as.data.frame()
-
-range(Asthma_agg_IR$`IR per 1000`)
-
-
-
-# Aggregating the National IR by year
- Asthma_agg_national_year_IR <- Asthma_agg %>%
-  group_by(Year) %>%
-  summarise_at(c("<12_month", "At_risk"), sum ) %>%
-  mutate(`IR per 1000` = `<12_month`/ At_risk*1000) %>%
-  as.data.frame()
-
- 
- 
- # Aggregating the National IR 
- Asthma_agg_national_IR <- Asthma_agg_national_year_IR %>%
-   #group_by(Year) %>%
-   summarize(`<12_month` = sum(`<12_month`)/5,
-             At_risk = sum(At_risk)/5,
-            `IR per 1000` = `<12_month`/ At_risk*1000) %>%
-   mutate(Year = "'National") %>% 
-   as.data.frame()
- 
- 
- # Printing Aggregated data to Excel
-write.xlsx(Asthma_agg_IR, "Results/Tables/Asthma_IR.xlsx", sheetName = "Aggregate", showNA=F, append = T, row.names = F)
-
-
 
 
 
@@ -134,36 +70,33 @@ write.xlsx(Asthma_agg_PRV, "Results/Tables/Asthma_PRV.xlsx", sheetName = "Aggreg
 
 
 
-
-
 # Creating tables ---------------------------------------------------------
 
 
-# Printing Table_A1; Asthma survey summaries by year
+# Printing Tables 8
 table_IR <- Asthma_agg_national_IR %>% 
   rbind(Asthma_agg_national_year_IR) 
-
+  
 table_PRV <- Asthma_agg_national_PRV %>% 
   rbind(Asthma_agg_national_year_PRV) 
 
 names <- table_IR$Year
 
-Table_A1 <- table_IR %>% 
+table_8 <- table_IR %>% 
   left_join(table_PRV, by = "Year") %>% 
   select(SAMPLE, EVER, `<12_month`, At_risk, `IR per 1000`, `PRV per 100`) %>% 
   t()
 
-colnames(Table_A1) <- names 
+colnames(table_8) <- names 
 
-write.xlsx(Table_A1, "Results/Tables/Table_A1.xlsx",  append = F, row.names = T)
+write.xlsx(table_8, "Results/Tables/Table_A1.xlsx",  append = F, row.names = T)
 
 
 
-# Printing Table_A2; Asthma survey summeries by state
+# Printing Table 9
 
-Table_A2 <- Asthma_agg_IR %>% 
+table_9 <- Asthma_agg_IR %>% 
   left_join(Asthma_agg_PRV, by = c("FIPS", "State")) %>% 
   select(State, SAMPLE, EVER, `<12_month`, At_risk, `IR per 1000`, `PRV per 100`)
 
-write.xlsx(Table_A2, "Results/Tables/Table_A2.xlsx",  append = F, row.names = F)
-
+write.xlsx(table_9, "Results/Tables/Table_A2.xlsx",  append = F, row.names = F)
